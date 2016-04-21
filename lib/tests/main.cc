768d1d6ac34e556f9bf3413aa590d36735e7b450
parisiale <alessandro@puppetlabs.com>
2016-04-21 20:32:44 +0100

    (maint) Allow enabling logging in tests/main.cc

--- lib/tests/main.cc
+++ lib/tests/main.cc
@@ -1,8 +1,22 @@
 #define CATCH_CONFIG_RUNNER
+
 #include <catch.hpp>
 
-int main(int argc, char **argv)
-{
-    return Catch::Session().run(argc, argv);
-}
+// To enable log messages:
+// #define ENABLE_LOGGING
 
+#ifdef ENABLE_LOGGING
+#include <boost/nowide/iostream.hpp>
+#define LEATHERMAN_LOGGING_NAMESPACE "puppetlabs.pcp-test.test"
+#include <leatherman/logging/logging.hpp>
+#endif
+
+int main(int argc, char** argv) {
+#ifdef ENABLE_LOGGING
+    leatherman::logging::setup_logging(boost::nowide::cout);
+    leatherman::logging::set_level(leatherman::logging::log_level::debug);
+#endif
+    Catch::Session test_session {};
+    test_session.configData().showDurations = Catch::ShowDurations::Always;
+    return test_session.run(argc, argv);
+}

2bcecf324b1db86f97c2ed5fabc19a97107099eb
parisiale <alessandro@puppetlabs.com>
2016-05-10 15:48:17 +0100

    (maint) Add docstrings to configuration functions

--- lib/inc/pcp-test/configuration.hpp
+++ lib/inc/pcp-test/configuration.hpp
@@ -15,8 +15,14 @@ namespace configuration {
 // Throw a std::out_of_range in case of invalid loglevel.
 void setup_logging(const std::string& logfile, const std::string& loglevel);
 
+// Parse the command line arguments and return an
+// application_options instance.
+// In case --help or --version were specified, display
+// the requested message and exit with EXIT_SUCCESS.
+// Throw a configuration_error in case of failure.
 application_options get_application_options(int argc, char** argv);
 
+// Validate the test_type argument.
 void validate_test_type(application_options& a_o);
 
 // Parse the configuration file.
@@ -26,6 +32,7 @@ void validate_test_type(application_options& a_o);
 // hardcoded value.
 void parse_configfile_and_process_options(application_options& a_o);
 
+// Throw a configuration_error in case of invalid options.
 void validate_application_options(application_options& a_o);
 
 }  // namespace configuration

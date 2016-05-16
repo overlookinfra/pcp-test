234e8cf285855ce7e6e3a45b6d513b65b80cb1a0
parisiale <alessandro@puppetlabs.com>
2016-05-16 12:28:53 +0100

    (PCP-409) Implement client methods to send test messages
    Implement methods to send requent, response, and error messages based on
    PCPClient::ParsedChunks and on the new 'message' class.

--- lib/inc/pcp-test/client.hpp
+++ lib/inc/pcp-test/client.hpp
@@ -6,8 +6,13 @@
 #pragma once
 
 #include <pcp-test/client_configuration.hpp>
+#include <pcp-test/message.hpp>
 
+#include <cpp-pcp-client/protocol/chunks.hpp>
 #include <cpp-pcp-client/connector/connector.hpp>
+
+#include <string>
+#include <vector>
 
 namespace pcp_test {
 
@@ -18,6 +23,20 @@ struct client
 
     client() = delete;
     explicit client(client_configuration config);
+
+    // Send the request message to the specified endpoints.
+    void send_request(const message& request,
+                      const std::vector<std::string>& endpoints);
+
+    // Replies to the specified request message with a
+    // response message.
+    void reply(const message& request);
+
+
+    // Replies to the specified request message with an
+    // error message.
+    void reply_with_error(const message& request,
+                          const std::string& err_msg);
 };
 
 }  // namespace pcp_test
--- lib/src/client.cc
+++ lib/src/client.cc
@@ -2,6 +2,8 @@
 
 #include <leatherman/logging/logging.hpp>
 
+#include <cpp-pcp-client/connector/errors.hpp>
+
 #include <utility>  // std::move
 
 namespace pcp_test {
@@ -16,6 +18,53 @@ client::client(client_configuration cfg)
                  configuration.connection_timeout_ms}
 {
     LOG_INFO("Instantiating client!");
+
+void client::send_request(const message& request,
+                  const std::vector<std::string>& endpoints)
+{
+    try {
+        connector.send(endpoints,
+                       schemas::REQUEST_TYPE,
+                       configuration.message_timeout_s,
+                       request.get_data());
+        LOG_DEBUG("Sent request %1%", request.transaction());
+    } catch (PCPClient::connection_error& e) {
+        LOG_ERROR("Failed to send request request %1%: %2%",
+                  request.transaction(), e.what());
+    }
+}
+
+void client::reply(const message& request)
+{
+    try {
+        connector.send({request.sender()},
+                       schemas::RESPONSE_TYPE,
+                       configuration.message_timeout_s,
+                       request.get_data());
+        LOG_DEBUG("Replied to request %1%", request.transaction());
+    } catch (PCPClient::connection_error& e) {
+        LOG_ERROR("Failed to reply to request %1%: %2%",
+                  request.transaction(), e.what());
+    }
+}
+
+void client::reply_with_error(const message& request,
+                              const std::string& err_msg)
+{
+    auto data = request.get_data();
+    data.set<std::string>("err_msg", err_msg);
+
+    try {
+        connector.send({request.sender()},
+                       schemas::ERROR_TYPE,
+                       configuration.message_timeout_s,
+                       std::move(data));
+        LOG_DEBUG("Replied to request %1% with an error", request.transaction());
+    } catch (PCPClient::connection_error& e) {
+        LOG_ERROR("Failed to reply to request %1% with an error: %2%",
+                  request.transaction(), e.what());
+    }
+}
 }
 
 }  // namespace pcp_test

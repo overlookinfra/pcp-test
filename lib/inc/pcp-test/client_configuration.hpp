ed73bc1975450b8dd745f513c60e80a78e57f35f
parisiale <alessandro@puppetlabs.com>
2016-05-16 11:36:31 +0100

    (PCP-408) Update unit tests

--- lib/inc/pcp-test/client_configuration.hpp
+++ lib/inc/pcp-test/client_configuration.hpp
@@ -10,14 +10,28 @@
 
 namespace pcp_test {
 
+extern const long DEFAULT_CONNECTION_TIMEOUT_MS;
+extern const unsigned int DEFAULT_MESSAGE_TIMEOUT_S;
+
 struct client_configuration
 {
-    std::vector<std::string> broker_ws_uris;
+    std::string common_name;
+    const std::string& client_type;
+    const std::vector<std::string>& broker_ws_uris;
+    const std::string& certificates_dir;
+    long connection_timeout_ms;
+    unsigned int message_timeout_s;
     std::string ca;
     std::string crt;
     std::string key;
-    std::string client_type;
-    long connection_timeout_ms;
+
+    client_configuration(
+            std::string common_name,
+            const std::string& client_type,
+            const std::vector<std::string>& broker_ws_uris,
+            const std::string& certificate_dir,
+            long connection_tmeout_ms = DEFAULT_CONNECTION_TIMEOUT_MS,
+            unsigned int message_timeout_s = DEFAULT_MESSAGE_TIMEOUT_S);
 };
 
 }  // namespace pcp_test
---
+++ lib/src/client_configuration.cc
@@ -0,0 +1,41 @@
+#include <pcp-test/client_configuration.hpp>
+
+#include <pcp-test/root_path.h>
+
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+
+#include <boost/format.hpp>
+
+#include <utility>
+
+namespace pcp_test {
+
+namespace fs = boost::filesystem;
+
+const long DEFAULT_CONNECTION_TIMEOUT_MS {100};
+const unsigned int DEFAULT_MESSAGE_TIMEOUT_S {5};
+
+static boost::format CERT_FILE_FORMAT {"%1%.example.com_%2%.pem"};
+
+client_configuration::client_configuration(
+        std::string common_name_,
+        const std::string& client_type_,
+        const std::vector <std::string>& broker_ws_uris_,
+        const std::string& certificates_dir_,
+        long connection_tmeout_ms_,
+        unsigned int message_timeout_s)
+    : common_name {std::move(common_name_)},
+      client_type {client_type_},
+      broker_ws_uris {broker_ws_uris_},
+      certificates_dir {certificates_dir_},
+      connection_timeout_ms {std::move(connection_tmeout_ms_)},
+      message_timeout_s {std::move(message_timeout_s)}
+{
+    fs::path c_p {certificates_dir};
+    ca  = (c_p / "ca_crt.pem").string();
+    crt = (c_p / "test" / (CERT_FILE_FORMAT % common_name % "crt").str()).string();
+    key = (c_p / "test" / (CERT_FILE_FORMAT % common_name % "key").str()).string();
+}
+
+}  // namespace pcp_test

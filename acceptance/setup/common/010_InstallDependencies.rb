09dd9f7b84bf43df26a6eacdbbbb5481213ad1ce
james-stocks <james.stocks@puppetlabs.com>
2016-09-09 10:49:07 +0000

    (PCP-579) Have acceptance test against the build from CI
    The initial commit of acceptance tests were just a placeholder, and they ran against a hardcoded URL to a pcp-test installer.
    This commit updates the acceptance setup to test against an environment specified build of pcp-test.
    Because pcp-test depends on the libraries of the the version of puppet-agent it was built against, this commit also updates the puppet agent installation
    to ensure that it installs the same version of puppet-agent that was used to build pcp-test.

--- acceptance/setup/common/010_InstallDependencies.rb
+++ acceptance/setup/common/010_InstallDependencies.rb
@@ -1,4 +1,7 @@
 step 'Install Puppet to assist with setup' do
+  hosts.each do |host|
+    install_puppetlabs_dev_repo(host, 'puppet-agent', ENV['PUPPET_AGENT_SHA'], 'repo-config')
+  end
   install_puppet_agent_on(hosts)
 end
 
